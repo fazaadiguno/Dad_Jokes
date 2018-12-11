6836810d916749de0669721753ffe7f217cfba0b
fazaadiguno <faza.adiguno@gmail.com>
2018-12-11 15:21:39 +0700

    Add files via upload

---
+++ precache-manifest.7b34569ca51a671f8f9a7f5949039b43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c170e1ee0dfc65793bbd",
+    "url": "/Dad_Jokes/static/js/runtime~main.c170e1ee.js"
+  },
+  {
+    "revision": "19bf88efa471ab3dedc0",
+    "url": "/Dad_Jokes/static/js/main.19bf88ef.chunk.js"
+  },
+  {
+    "revision": "d7c704c15d429fde6cf2",
+    "url": "/Dad_Jokes/static/js/1.d7c704c1.chunk.js"
+  },
+  {
+    "revision": "19bf88efa471ab3dedc0",
+    "url": "/Dad_Jokes/static/css/main.5fb729bf.chunk.css"
+  },
+  {
+    "revision": "3c157476e60dd960ed1c92274dcaf988",
+    "url": "/Dad_Jokes/index.html"
+  }
+];
